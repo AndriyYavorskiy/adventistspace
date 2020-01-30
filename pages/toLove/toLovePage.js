aecec99bfc16e1041257f8fc708f36273103333a
Andrii Yavorskyi <irey1@ukr.net>
2020-01-31 00:38:58 +0200

    Fixed 2love page localization

--- pages/toLove/toLovePage.js
+++ pages/toLove/toLovePage.js
@@ -1,30 +1,25 @@
-angular.module('AMO').controller('2LovePageController', ['$scope', '$interval', function ($scope, $interval) {
+angular.module('AMO').controller('2LovePageController', ['$scope', '$interval', '$filter', function ($scope, $interval, $filter) {
   var targetDate = new Date();
-  targetDate.setFullYear(2020);
-  targetDate.setMonth(1);
-  targetDate.setDate(20);
+  targetDate.setYear(2020);
+  targetDate.setMonth(1, 20);
   targetDate.setHours(20);
   targetDate.setMinutes(20);
   targetDate.setSeconds(20);
+  var allSeconds = Math.round((targetDate.valueOf() - new Date().valueOf()) / 1000);
   $scope.timeLeft = '';
 
-
   $interval(function() {
-    $scope.timeLeft = getTimeLeft();
-  }, 1000);
+    allSeconds--;
 
-  function getTimeLeft () {
-    var secondsLeft = Math.round((targetDate.valueOf() - new Date().valueOf()) / 1000);
-    
-    return getDisplayTime(secondsLeft);
-  }
-
-  function getDisplayTime(allSeconds) {
     const fullDays = Math.floor(allSeconds / 86400);
     const restSeconds = allSeconds % 86400;
     const hours =  Math.floor(restSeconds / 3600);
-    const minutes =  `00${Math.floor((restSeconds % 3600) / 60)}`.substr(-2);
-    const seconds = `00${Math.floor(restSeconds % 60)}`.substr(-2);
-    return `${fullDays} (полных суток) ${hours}:${minutes}:${seconds}`;
-  }
+    const minutes = ('00' + Math.floor((restSeconds % 3600) / 60)).substr(-2);
+    const seconds = ('00' + Math.floor(restSeconds % 60)).substr(-2);
+    $scope.translationData = {
+      days: fullDays,
+      parts: hours + ':' + minutes + ':' + seconds
+    };
+  }, 1000);
+
 }]);
